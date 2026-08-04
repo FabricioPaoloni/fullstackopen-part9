23e6d9b9f27e2a45b69baa6a84de845a533fc373
Fabricio Paoloni <paolonifabricio@gmail.com>
2026-08-03 21:54:59 -0300

    Exercise 9.8: Checkup -COMPLETED-

--- healthapp/exerciseCalculator.ts
+++ healthapp/exerciseCalculator.ts
@@ -15,7 +15,7 @@ export const calculateExercises = (
   target: number,
 ): returnObjectFormat => {
   //   let returnObject = {};
-  console.log(userDataArray);
+  // console.log(userDataArray);
   const periodLength = userDataArray.length;
   const trainingDays = userDataArray.filter((value) => value > 0).length;
   const average =
@@ -50,43 +50,47 @@ export const calculateExercises = (
   return returnObject;
 };
 
-//like in the example, the first argument is the target
-const target: number = Number(process.argv[2]);
-const day1: number | null = process.argv[3] ? Number(process.argv[3]) : null;
-const day2: number | null = process.argv[4] ? Number(process.argv[4]) : null;
-const day3: number | null = process.argv[5] ? Number(process.argv[5]) : null;
-const day4: number | null = process.argv[6] ? Number(process.argv[6]) : null;
-const day5: number | null = process.argv[7] ? Number(process.argv[7]) : null;
-const day6: number | null = process.argv[8] ? Number(process.argv[8]) : null;
-const day7: number | null = process.argv[9] ? Number(process.argv[9]) : null;
-const day8: number | null = process.argv[10] ? Number(process.argv[10]) : null;
+if (process.argv[1] === import.meta.filename) {
+  //like in the example, the first argument is the target
+  const target: number = Number(process.argv[2]);
+  const day1: number | null = process.argv[3] ? Number(process.argv[3]) : null;
+  const day2: number | null = process.argv[4] ? Number(process.argv[4]) : null;
+  const day3: number | null = process.argv[5] ? Number(process.argv[5]) : null;
+  const day4: number | null = process.argv[6] ? Number(process.argv[6]) : null;
+  const day5: number | null = process.argv[7] ? Number(process.argv[7]) : null;
+  const day6: number | null = process.argv[8] ? Number(process.argv[8]) : null;
+  const day7: number | null = process.argv[9] ? Number(process.argv[9]) : null;
+  const day8: number | null = process.argv[10]
+    ? Number(process.argv[10])
+    : null;
 
-if (day8 !== null) {
-  console.log("Too many days for a week :/ Maximun is 7");
-} else {
-  let inputError = false;
-  const userArray = [day1, day2, day3, day4, day5, day6, day7, day8];
-  //we check for target input first
-  if (isNaN(target)) {
-    inputError = true;
-    //we change this value to print an error if the user introduced wrong values
-  }
-  userArray.map((day) => {
-    if (isNaN(Number(day)) || Number(day) > 7) {
-      inputError = true;
-    }
-  });
-  //we could improve the validations warranting that the number of hours don't exceed some logical number like 8 or 12 hours of training at most. I'll leave that for later
-  if (inputError) {
-    console.log("Data introduced is wrong, only numbers are accepted");
+  if (day8 !== null) {
+    console.log("Too many days for a week :/ Maximun is 7");
   } else {
-    const userArrayFiltered = userArray.filter((day) => day !== null);
-    if (userArrayFiltered.length === 0) {
-      console.log(
-        "No training days passed. Introduce at least information for day1, introduce 0 if you want to express 0 hours of training that day or any day.",
-      );
+    let inputError = false;
+    const userArray = [day1, day2, day3, day4, day5, day6, day7, day8];
+    //we check for target input first
+    if (isNaN(target)) {
+      inputError = true;
+      //we change this value to print an error if the user introduced wrong values
+    }
+    userArray.map((day) => {
+      if (isNaN(Number(day)) || Number(day) > 7) {
+        inputError = true;
+      }
+    });
+    //we could improve the validations warranting that the number of hours don't exceed some logical number like 8 or 12 hours of training at most. I'll leave that for later
+    if (inputError) {
+      console.log("Data introduced is wrong, only numbers are accepted");
     } else {
-      console.log(calculateExercises(userArrayFiltered, target));
+      const userArrayFiltered = userArray.filter((day) => day !== null);
+      if (userArrayFiltered.length === 0) {
+        console.log(
+          "No training days passed. Introduce at least information for day1, introduce 0 if you want to express 0 hours of training that day or any day.",
+        );
+      } else {
+        console.log(calculateExercises(userArrayFiltered, target));
+      }
     }
   }
 }
--- healthapp/index.ts
+++ healthapp/index.ts
@@ -15,31 +15,44 @@ app.get("/bmi", (req, res) => {
   const { height, weight } = req.query;
 
   //check for height and weight number types (type validation)
-  if (!height || typeof height !== "string") {
-    throw new Error("la variable height debe ser un número");
+  if (!height) {
+    res.status(400).json({ error: "malformatted parameters" });
+  }
+  if (!weight) {
+    res.status(400).json({ error: "malformatted parameters" });
+  }
+  if (typeof height !== "string") {
+    throw new Error("malformatted parameters");
     // res.status(500).json({ error: "la variable height debe ser un número" });
   }
-  if (!weight || typeof weight !== "string") {
-    throw new Error("la variable weight debe ser un número");
+  if (typeof weight !== "string") {
+    throw new Error("malformatted parameters");
     // res.status(500).json({ error: "la variable weight debe ser un número" });
   }
   if (isNaN(parseInt(height))) {
-    res.status(500).json({ error: "la variable height debe ser un número" });
+    res.status(400).json({ error: "malformatted parameters" });
   }
   if (isNaN(parseInt(weight))) {
-    res.status(500).json({ error: "la variable weight debe ser un número" });
+    res.status(400).json({ error: "malformatted parameters" });
   }
   //validation ends
+  const heightParsed = parseInt(height);
+  const weightParsed = parseInt(weight);
 
-  const bmiResult = calculateBmi(parseInt(height), parseInt(weight));
+  const bmiResult = calculateBmi(heightParsed, weightParsed);
 
-  console.log(height, weight, bmiResult);
-  res.json({ weight, height, bmi: bmiResult });
+  // console.log(height, weight, bmiResult);
+
+  res.json({ weight: weightParsed, height: heightParsed, bmi: bmiResult });
 });
 
 app.post("/exercises", (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   const { daily_exercises, target } = req.body;
+  if (!target || !daily_exercises) {
+    res.status(400).json({ error: "parameters missing" });
+  }
+
   // console.log(daily_exercises);
   const day1: number | null =
     daily_exercises[0] !== undefined ? Number(daily_exercises[0]) : null;
@@ -59,22 +72,22 @@ app.post("/exercises", (req, res) => {
     daily_exercises[7] !== undefined ? Number(daily_exercises[7]) : null;
 
   if (day8 !== null) {
-    res.status(400).send("Too many days for a week :/ Maximun is 7");
+    res.status(400).json({ error: "Too many days for a week :/ Maximun is 7" });
   } else {
     let inputError = false;
     const userArray = [day1, day2, day3, day4, day5, day6, day7, day8];
     //we check for target input first
     if (isNaN(target)) {
-      inputError = true;
+      res.status(400).json({ error: "malformatted parameters" });
       //we change this value to print an error if the user introduced wrong values
     }
-    if (target < 0 || target > 12) {
-      res
-        .status(400)
-        .send(
-          "Target must be between 0 and 12, it represent an average number of training hours per day",
-        );
-    }
+    // if (target < 0 || target > 12) {
+    //   res
+    //     .status(400)
+    //     .send(
+    //       "Target must be between 0 and 12, it represent an average number of training hours per day",
+    //     );
+    // }
     userArray.map((day) => {
       if (isNaN(Number(day)) || Number(day) > 12) {
         inputError = true;
@@ -82,27 +95,20 @@ app.post("/exercises", (req, res) => {
     });
 
     if (inputError) {
-      res
-        .status(400)
-        .send(
-          "Data introduced is wrong, only numbers between 0 and 12 are accepted",
-        );
+      res.status(400).json({ error: "malformatted parameters" });
     } else {
-      console.log(userArray);
+      // console.log(userArray);
       const userArrayFiltered = userArray.filter((day) => day !== null);
       if (userArrayFiltered.length === 0) {
-        res
-          .status(400)
-          .send(
-            "No training days passed. Introduce at least information for day1, introduce 0 if you want to express 0 hours of training that day or any day.",
-          );
+        res.status(400).json({ error: "parameters missing" });
       } else {
         res.json(calculateExercises(userArrayFiltered, target));
       }
     }
   }
 });
-const PORT = 3003;
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
