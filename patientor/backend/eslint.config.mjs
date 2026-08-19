0369c5515a06b950cb091a4642b1308f0fab5d2b
Fabricio Paoloni <paolonifabricio@gmail.com>
2026-08-18 22:10:09 -0300

    Uninstalled typescript v7, reinstalled typescript v6 and installed typescript-eslint which was causing an error with TS v7

--- patientor/backend/eslint.config.mjs
+++ patientor/backend/eslint.config.mjs
@@ -1,6 +1,6 @@
 import eslint from "@eslint/js";
-// import tseslint from "typescript-eslint";
-import stylistic from '"stylistic/eslint-plugin';
+import tseslint from "typescript-eslint";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default tseslint.config({
   files: ["**/*.ts"],
@@ -19,13 +19,13 @@ export default tseslint.config({
   },
   rules: {
     "@stylistic/semi": "error",
-    // "@typescript-eslint/no-unsafe-assignment": "error",
-    // "@typescript-eslint/no-explicit-any": "error",
-    // "@typescript-eslint/explicit-function-return-type": "off",
-    // "@typescript-eslint/explicit-module-boundary-types": "off",
-    // "@typescript-eslint/restrict-template-expressions": "off",
-    // "@typescript-eslint/restrict-plus-operands": "off",
-    // "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-    // "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/no-unsafe-assignment": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/restrict-plus-operands": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/consistent-type-imports": "error",
   },
 });
