f154bb78b26233714d158193c129b4bfe6a97ac4
Fabricio Paoloni <paolonifabricio@gmail.com>
2026-08-18 21:55:08 -0300

    Exercise 9.16 - desactivated typescript-eslint import and typescript-eslint rule in eslint.config.mjs because of typescript v7 incompatibility

--- patientor/backend/eslint.config.mjs
+++ patientor/backend/eslint.config.mjs
@@ -1,5 +1,5 @@
 import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+// import tseslint from "typescript-eslint";
 import stylistic from '"stylistic/eslint-plugin';
 
 export default tseslint.config({
@@ -19,13 +19,13 @@ export default tseslint.config({
   },
   rules: {
     "@stylistic/semi": "error",
-    "@typescript-eslint/no-unsafe-assignment": "error",
-    "@typescript-eslint/no-explicit-any": "error",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/restrict-template-expressions": "off",
-    "@typescript-eslint/restrict-plus-operands": "off",
-    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/consistent-type-imports": "error",
+    // "@typescript-eslint/no-unsafe-assignment": "error",
+    // "@typescript-eslint/no-explicit-any": "error",
+    // "@typescript-eslint/explicit-function-return-type": "off",
+    // "@typescript-eslint/explicit-module-boundary-types": "off",
+    // "@typescript-eslint/restrict-template-expressions": "off",
+    // "@typescript-eslint/restrict-plus-operands": "off",
+    // "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    // "@typescript-eslint/consistent-type-imports": "error",
   },
 });
